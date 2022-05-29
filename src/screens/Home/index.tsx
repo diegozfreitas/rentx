29c3b49443c68d8d799ab5b61a7bcede8f945173
Diego ZF <ddzadravec@gmail.com>
2022-05-29 00:43:38 -0300

    feat: start project add typescript add files assets config styled-component

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+import { Container, Title } from "./styles";
+
+export const Home = () => {
+  return (
+    <Container>
+      <View>
+        <Title>Oi</Title>
+      </View>
+    </Container>
+  );
+};
