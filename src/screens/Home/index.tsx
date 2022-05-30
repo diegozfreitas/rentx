5b0aeca055c068702d2368a92132b586a253f91b
Diego ZF <ddzadravec@gmail.com>
2022-05-29 22:56:21 -0300

    feat: create component Car

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -6,7 +6,19 @@ import Logo from "../../assets/logo.svg";
 
 import { Container, Header, HeaderContent, TotalCars } from "./styles";
 
+import { Car } from "../../components/Car";
+
 export const Home = () => {
+  const data = {
+    brand: "Audi",
+    name: "RCCoupé",
+    rent: {
+      period: "Ao dia",
+      price: 140.0,
+    },
+    thumbnail:
+      "https://image.webmotors.com.br/_fotos/AnuncioUsados/gigante/2022/202203/20220302/AUDI-A5-1.8-TFSI-SPORTBACK-AMBIENTE-16V-GASOLINA-4P-MULTITRONIC-wmimagem15222229826.jpg",
+  };
   return (
     <Container>
       <StatusBar
@@ -22,6 +34,9 @@ export const Home = () => {
           <TotalCars>Total de 12 carros</TotalCars>
         </HeaderContent>
       </Header>
+
+      <Car data={data}/>
+      <Car data={data}/>
     </Container>
   );
 };
