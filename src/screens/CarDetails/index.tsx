cd4e3bd5d6fdc7b35d2ae74ed56bef40e3e26c0b
Diego ZF <ddzadravec@gmail.com>
2022-05-30 19:26:54 -0300

    feat: add ImageSlider comp.. in screen carDetails

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -2,20 +2,28 @@ import React from "react";
 import { View, Text } from "react-native";
 
 import { BackButton } from "../../components/BackButton";
+import { ImageSlider } from "../../components/ImageSlider";
 
-import { Container, Header } from "./style";
+import { Container, Header, CarImages } from "./style";
 
 export const CarDetails = () => {
   return (
     <Container>
       <Header>
-        <Text>Oi</Text>
         <BackButton
           onPress={() => {
             console.log("oi");
           }}
         />
       </Header>
+
+      <CarImages>
+        <ImageSlider
+          imagesUrl={[
+            "https://image.webmotors.com.br/_fotos/AnuncioUsados/gigante/2022/202203/20220302/AUDI-A5-1.8-TFSI-SPORTBACK-AMBIENTE-16V-GASOLINA-4P-MULTITRONIC-wmimagem15222229826.jpg",
+          ]}
+        />
+      </CarImages>
     </Container>
   );
 };
