c676a2a7bed5621de9e613d4ee50a43b90ac1d83
Diego ZF <ddzadravec@gmail.com>
2022-05-30 19:56:11 -0300

    feat: create content details and about text

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,10 +1,22 @@
 import React from "react";
-import { View, Text } from "react-native";
 
 import { BackButton } from "../../components/BackButton";
 import { ImageSlider } from "../../components/ImageSlider";
 
-import { Container, Header, CarImages } from "./style";
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About
+} from "./style";
 
 export const CarDetails = () => {
   return (
@@ -24,6 +36,26 @@ export const CarDetails = () => {
           ]}
         />
       </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborguini</Brand>
+            <Name>Huraran</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao Dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <About>
+          Este é automóvel desportivo. Surgiu do lendário touro de lide
+          indultado na praça Real Maestranza de Sevilla. É um belíssimo carro
+          para quem gosta de acelerar.
+        </About>
+      </Content>
     </Container>
   );
 };
