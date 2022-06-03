3ee8270c8b12296c7357711988cbe155385b26d1
Diego ZF <ddzadravec@gmail.com>
2022-06-03 14:48:15 -0300

    feat: create page Schedule and header

---
+++ src/screens/Scheduling/index.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { StatusBar } from "react-native";
+import { useTheme } from "styled-components";
+
+import ArrowSvg from "../../assets/arrow.svg";
+
+import { Button } from "../../components/Button";
+import { BackButton } from "../../components/BackButton";
+
+import {
+  Container,
+  Header,
+  Title,
+  RentalPeriod,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  Content,
+  Footer,
+} from "./style";
+
+export const Scheduling = () => {
+  const theme = useTheme();
+
+  return (
+    <Container>
+      <Header>
+        <StatusBar
+          barStyle={"light-content"}
+          translucent
+          backgroundColor={"transparent"}
+        />
+        <BackButton onPress={() => {}} color={theme.colors.shape} />
+
+        <Title>
+          Escolha uma {"\n"}data de inicio e {"\n"}fim do aluguel
+        </Title>
+
+        <RentalPeriod>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue selected={false}></DateValue>
+          </DateInfo>
+
+          <ArrowSvg />
+
+          <DateInfo>
+            <DateTitle>ATÉ</DateTitle>
+            <DateValue selected={true}>18/06/2021</DateValue>
+          </DateInfo>
+        </RentalPeriod>
+      </Header>
+
+      <Content></Content>
+
+      <Footer>
+        <Button title="CONFIRMAR" onPress={() => {}} />
+      </Footer>
+    </Container>
+  );
+};
