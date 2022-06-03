159b0344d193875423885d98e09a5a3641987d3f
Diego ZF <ddzadravec@gmail.com>
2022-06-03 17:03:03 -0300

    feat: create page ScheduleComplete

---
+++ src/screens/SchedulingComplete/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useWindowDimensions } from "react-native";
+
+import LogoSvg from "../../assets/logo_background_gray.svg";
+import DoneSvg from "../../assets/done.svg";
+
+import { ConfirmButton } from "../../components/ConfirmButton";
+
+import { Container, Content, Title, Message, Footer } from "./style";
+
+export const SchedulingComplete = () => {
+  const { width } = useWindowDimensions();
+
+  return (
+    <Container>
+      <LogoSvg width={width} />
+
+      <Content>
+        <DoneSvg width={80} />
+
+        <Title>Carro alugado!</Title>
+
+        <Message>
+          Agora você só precisa ir{"\n"}a concessionária da RENTX{"\n"}
+          pegar seu automóvel
+        </Message>
+      </Content>
+
+      <Footer>
+        <ConfirmButton title="ok" onPress={() => {}} />
+      </Footer>
+    </Container>
+  );
+};
