d5bdb45cdddc31c90be5c756f6ed4b7e993f7c09
Diego ZF <ddzadravec@gmail.com>
2022-08-16 23:17:20 -0300

    feat: Finish screen MyCars

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,14 +1,120 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { View, Text, StatusBar, FlatList } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { useTheme } from "styled-components";
+import { AntDesign } from "@expo/vector-icons";
+import { RFValue } from "react-native-responsive-fontsize";
 
-import { Container } from './style';
+import {
+  Container,
+  Header,
+  Title,
+  SubTitle,
+  Content,
+  AppointmentsContent,
+  AppointmentsTitle,
+  AppointmentsQuantity,
+  CarWrapper,
+  CarFooter,
+  CarFooterTitle,
+  CarFooterPeriod,
+  CarFooterDate,
+} from "./style";
+
+import { BackButton } from "../../components/BackButton";
+import { Car } from "../../components/Car";
+import { Load } from "../../components/Load";
+
+import { CarDto } from "../../dtos/carDtos";
+import { api } from "../../services/api";
+
+interface CarProps {
+  id: string;
+  car: CarDto[];
+  user_id: string;
+  startDate: string;
+  endDate: string;
+}
 
 export const MyCars = () => {
+  const [cars, setCars] = useState<CarProps[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const theme = useTheme();
+  const navigation = useNavigation();
+
+  const handleBack = () => {
+    navigation.goBack();
+  };
+
+  useEffect(() => {
+    setLoading(true);
+
+    (async () => {
+      try {
+        const response = await api.get("schedules_byuser?user_id=1");
+        setCars(response.data);
+        setLoading(false);
+      } catch (err) {
+        console.log(err);
+        setLoading(false);
+      }
+    })();
+  }, []);
+
   return (
     <Container>
-      <View>
-        <Text>Oi</Text>
-      </View>
+      <Header>
+        <StatusBar
+          barStyle={"light-content"}
+          translucent
+          backgroundColor={"transparent"}
+        />
+        <BackButton onPress={handleBack} color={theme.colors.shape} />
+
+        <Title>Seus agendamentos {"\n"}estão aqui.</Title>
+
+        <SubTitle>Conforto, segurança e praticidade</SubTitle>
+      </Header>
+
+      {loading ? (
+        <Load />
+      ) : (
+        <Content>
+          <AppointmentsContent>
+            <AppointmentsTitle>
+              {cars.length === 1 ? "Agendamento feito" : "Agendamentos feitos"}{" "}
+            </AppointmentsTitle>
+            <AppointmentsQuantity>0{cars.length}</AppointmentsQuantity>
+          </AppointmentsContent>
+
+          <FlatList
+            data={cars}
+            keyExtractor={(item) => item.id}
+            showsVerticalScrollIndicator={false}
+            renderItem={({ item }) => (
+              <CarWrapper>
+                <Car data={item.car} />
+                <CarFooter>
+                  <CarFooterTitle>Período</CarFooterTitle>
+                  <CarFooterPeriod>
+                    <CarFooterDate>{item.startDate}</CarFooterDate>
+
+                    <AntDesign
+                      name="arrowright"
+                      size={RFValue(16)}
+                      color={theme.colors.title}
+                      style={{ marginHorizontal: 10 }}
+                    />
+
+                    <CarFooterDate>{item.endDate}</CarFooterDate>
+                  </CarFooterPeriod>
+                </CarFooter>
+              </CarWrapper>
+            )}
+          />
+        </Content>
+      )}
     </Container>
   );
 };
--- src/screens/MyCars/style.ts
+++ src/screens/MyCars/style.ts
@@ -1,3 +1,95 @@
-import styled from 'styled-components/native';
+import styled from "styled-components/native";
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import { RFValue } from "react-native-responsive-fontsize";
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.background_primary};
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  height: 280px;
+  background-color: ${({ theme }) => theme.colors.header};
+
+  justify-content: space-between;
+  padding: 25px 20px;
+  padding-top: ${getStatusBarHeight() + 20}px;
+`;
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-family: ${({ theme }) => theme.fonts.secondary600};
+  font-size: ${RFValue(30)}px;
+  letter-spacing: 1px;
+
+  margin-top: 8px;
+`;
+
+export const SubTitle = styled.Text`
+  color: ${({ theme }) => theme.colors.shape};
+  font-family: ${({ theme }) => theme.fonts.secondary400};
+  font-size: ${RFValue(15)}px;
+
+  margin-top: 8px;
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  width: 100%;
+  padding: 0px 16px;
+`;
+
+export const AppointmentsContent = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 24px 0px;
+`;
+
+export const AppointmentsTitle = styled.Text`
+  color: ${({ theme }) => theme.colors.text};
+  font-family: ${({ theme }) => theme.fonts.secondary400};
+  font-size: ${RFValue(15)}px;
+`;
+
+export const AppointmentsQuantity = styled.Text`
+  color: ${({ theme }) => theme.colors.title};
+  font-family: ${({ theme }) => theme.fonts.secondary500};
+  font-size: ${RFValue(15)}px;
+`;
+
+export const CarWrapper = styled.View`
+  margin-bottom: 16px;
+`;
+
+export const CarFooter = styled.View`
+  width: 100%;
+  padding: 12px;
+
+  margin-top: -10px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  background-color: ${({ theme }) => theme.colors.background_secondary};
+`;
+
+export const CarFooterTitle = styled.Text`
+  color: ${({ theme }) => theme.colors.text_details};
+  font-family: ${({ theme }) => theme.fonts.secondary500};
+  font-size: ${RFValue(10)}px;
+`;
+
+export const CarFooterPeriod = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const CarFooterDate = styled.Text`
+  color: ${({ theme }) => theme.colors.title};
+  font-family: ${({ theme }) => theme.fonts.secondary400};
+  font-size: ${RFValue(13)}px;
+`;
