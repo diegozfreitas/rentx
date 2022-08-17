3286a41db891a758e8cacc06b5a21f31d765d29a
Diego ZF <ddzadravec@gmail.com>
2022-08-16 22:10:36 -0300

    feat: Start MyCars screen

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import { Home } from "../screens/Home";
+import { MyCars } from "../screens/MyCars";
 import { CarDetails } from "../screens/CarDetails";
 import { Scheduling } from "../screens/Scheduling";
 import { SchedulingDetails } from "../screens/SchedulingDetails";
@@ -13,6 +14,7 @@ export const StackRoutes = () => {
   return (
     <Navigator screenOptions={{ headerShown: false }}>
       <Screen name="Home" component={Home} />
+      <Screen name="MyCars" component={MyCars} />
       <Screen name="CarDetails" component={CarDetails} />
       <Screen name="Scheduling" component={Scheduling} />
       <Screen name="SchedulingDetails" component={SchedulingDetails} />
---
+++ src/screens/MyCars/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import { Container } from './style';
+
+export const MyCars = () => {
+  return (
+    <Container>
+      <View>
+        <Text>Oi</Text>
+      </View>
+    </Container>
+  );
+};
